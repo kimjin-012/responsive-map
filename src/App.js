d91e94d87471ce622811da6cbb02874e90b37738
kimjin-012 <77659317+kimjin-012@users.noreply.github.com>
2021-06-10 23:16:20 -0700

    Basic map complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,174 @@
-import {useEffect, useState, useRef} from 'react'
-import './App.css';
-import * as tt from '@tomtom-international/web-sdk-maps';
+import { useRef, useEffect, useState } from 'react'
+import * as tt from '@tomtom-international/web-sdk-maps'
+import * as ttapi from '@tomtom-international/web-sdk-services'
+import './App.css'
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
+
+import Logo from './image/direction.png'
 
 const App = () => {
   const mapElement = useRef()
   const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(-122.335167)
+  const [latitude, setLatitude] = useState(47.608013)
 
-  // useEffect(() => {
-  //   let map = tt.map({
-  //     key: process.env.MAP_API_KEY,
-  //     container: mapElement.current,
-  //   })
-  //   setMap(map)
+  const convertToPoints = (lngLat) => {
+    return {
+      point: {
+        latitude: lngLat.lat,
+        longitude: lngLat.lng
+      }
+    }
+  }
 
-  // }, [])
+  const routes = (geoJson, map) => {
+    if (map.getLayer('route')) {
+      map.removeLayer('route')
+      map.removeSource('route')
+    }
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geoJson
+      },
+      paint: {
+        'line-color': 'red',
+        'line-width': 6
+  
+      }
+    })
+  }
+
+  const deliveryMarker = (lngLat, map) => {
+    const element = document.createElement('div')
+    element.className = 'marker-delivery'
+    new tt.Marker({
+      element: element
+    })
+    .setLngLat(lngLat)
+    .addTo(map)
+  }
 
   useEffect(() => {
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    }
+    const destinations = []
+
     let map = tt.map({
       key: process.env.REACT_APP_API_KEY,
       container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [longitude, latitude],
+      zoom: 14,
     })
-
     setMap(map)
 
-  }, [])
+    const Marker = () => {
+      const popupOffset = {
+        bottom: [0, -25]
+      }
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML('This is you!')
+      const element = document.createElement('div')
+      element.className = 'marker'
+
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(map)
+      
+      marker.on('dragend', () => {
+        const lngLat = marker.getLngLat()
+        setLongitude(lngLat.lng)
+        setLatitude(lngLat.lat)
+      })
+
+      marker.setPopup(popup).togglePopup()
+      
+    }
+    Marker()
+
+    const destinationSort = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+      const callParameters = {
+        key: process.env.REACT_APP_API_KEY,
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      }
+
+    return new Promise((resolve, reject) => {
+      ttapi.services
+        .matrixRouting(callParameters)
+        .then((matrixAPIResults) => {
+          const results = matrixAPIResults.matrix[0]
+          const resultsArray = results.map((result, index) => {
+            return {
+              location: locations[index],
+              drivingtime: result.response.routeSummary.travelTimeInSeconds,
+            }
+          })
+          resultsArray.sort((a, b) => {
+            return a.drivingtime - b.drivingtime
+          })
+          const sortedLocations = resultsArray.map((result) => {
+            return result.location
+          })
+          resolve(sortedLocations)
+        })
+      })
+    }
+
+    const routeCalculation = () => {
+      destinationSort(destinations).then((sorted) => {
+        sorted.unshift(origin)
+
+        ttapi.services
+          .calculateRoute({
+            key: process.env.REACT_APP_API_KEY,
+            locations: sorted,
+          })
+          .then((routeData) => {
+            const geoJson = routeData.toGeoJson()
+            routes(geoJson, map)
+        })
+      })
+    }
+
+
+    map.on('click', e => {
+      destinations.push(e.lngLat)
+      deliveryMarker(e.lngLat, map)
+      routeCalculation()
+    })
+
+    return () => map.remove()
+  }, [longitude, latitude])
 
   return (
-    <div className="App">
-      <div ref={mapElement}></div>
-    </div>
-  );
+    <>
+        <div className="App">
+          <img src={Logo} alt="Logo" />
+          <div className="search-bar">
+            <h1>Your Location??</h1>
+            <label htmlFor="longtitude">Longitude: </label>
+            <input type="text" id="longitude" className="longitude" onChange={e => {setLongitude(e.target.value)}}/>
+            <label htmlFor="latitude">Latitude:</label>
+            <input type="text" id="latitude" className="latitude" onChange={e => {setLatitude(e.target.value)}}/>
+          </div>
+          <div ref={mapElement} className="map" />
+        </div>
+    </>
+  )
 }
 
 export default App;
